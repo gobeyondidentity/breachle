b4a9acfc17861a0408d28e0bff2d4480bc9801c5
Nelson Melo <nmelo.cu@gmail.com>
2023-04-14 18:44:54 -0400

    increased body size

--- breaches.js
+++ breaches.js
@@ -1,113 +1,113 @@
 const breaches = [
   {
-      name: "Equifax",
-      clues: [
-          "September 2017, United States, $4 billion",
-          "Credit reporting agency",
-          "Affected approximately 147 million people",
-          "Exposed Social Security numbers, birth dates, addresses, and more",
-          "Company settled with the Federal Trade Commission for up to $700 million"
-      ]
+    name: "Equifax",
+    clues: [
+        "September 2017, United States, $4 billion",
+        "Credit reporting agency",
+        "Affected approximately 147 million people",
+        "Exposed Social Security numbers, birth dates, addresses, and more",
+        "Company settled with the Federal Trade Commission for up to $700 million"
+    ]
   },
   {
-      name: "Yahoo",
-      clues: [
-          "December 2013, United States, $350 million",
-          "Internet services company",
-          "Affected 3 billion user accounts",
-          "Exposed names, email addresses, phone numbers, and more",
-          "Largest known data breach in history"
-      ]
+    name: "Yahoo",
+    clues: [
+        "December 2013, United States, $350 million",
+        "Internet services company",
+        "Affected 3 billion user accounts",
+        "Exposed names, email addresses, phone numbers, and more",
+        "Largest known data breach in history"
+    ]
   },
   {
-      name: "Marriott",
-      clues: [
-          "November 2018, United States, $124 million",
-          "Hotel chain",
-          "Affected around 500 million guests",
-          "Exposed names, addresses, passport numbers, and more",
-          "Starwood Hotels and Resorts database was compromised"
-      ]
+    name: "Marriott",
+    clues: [
+        "November 2018, United States, $124 million",
+        "Hotel chain",
+        "Affected around 500 million guests",
+        "Exposed names, addresses, passport numbers, and more",
+        "Starwood Hotels and Resorts database was compromised"
+    ]
   },
   {
-      name: "First American Financial",
-      clues: [
-          "May 2019, United States, $487 million",
-          "Real estate services company",
-          "Exposed around 885 million records",
-          "Exposed bank account numbers, Social Security numbers, and more",
-          "Data leak due to a website design flaw"
-      ]
+    name: "First American Financial",
+    clues: [
+        "May 2019, United States, $487 million",
+        "Real estate services company",
+        "Exposed around 885 million records",
+        "Exposed bank account numbers, Social Security numbers, and more",
+        "Data leak due to a website design flaw"
+    ]
   },
   {
-      name: "Adult FriendFinder",
-      clues: [
-          "October 2016, United States",
-          "Online dating and adult entertainment platform",
-          "Affected around 412 million user accounts",
-          "Exposed usernames, passwords, and more",
-          "Largest known data breach of a dating platform"
-      ]
+    name: "Adult FriendFinder",
+    clues: [
+        "October 2016, United States",
+        "Online dating and adult entertainment platform",
+        "Affected around 412 million user accounts",
+        "Exposed usernames, passwords, and more",
+        "Largest known data breach of a dating platform"
+    ]
   },
   {
-      name: "Anthem",
-      clues: [
-          "February 2015, United States, $260 million",
-          "Health insurance company",
-          "Affected around 78.8 million individuals",
-          "Exposed Social Security numbers, dates of birth, addresses, and more",
-          "Largest known healthcare data breach"
-      ]
+    name: "Anthem",
+    clues: [
+        "February 2015, United States, $260 million",
+        "Health insurance company",
+        "Affected around 78.8 million individuals",
+        "Exposed Social Security numbers, dates of birth, addresses, and more",
+        "Largest known healthcare data breach"
+    ]
   },
   {
-      name: "Target",
-      clues: [
-          "December 2013, United States, $252 million",
-          "Retail corporation",
-          "Affected around 110 million individuals",
-          "Exposed credit and debit card information, customer names, and more",
-          "Point-of-sale malware attack"
-      ]
+    name: "Target",
+    clues: [
+        "December 2013, United States, $252 million",
+        "Retail corporation",
+        "Affected around 110 million individuals",
+        "Exposed credit and debit card information, customer names, and more",
+        "Point-of-sale malware attack"
+    ]
   },
   {
-      name: "Sony Pictures",
-      clues: [
-          "November 2014, United States, $100 million",
-          "Film production company",
-          "Exposed unreleased movies, confidential information, and more",
-          "Attributed to North Korea, possibly in retaliation for the film 'The Interview'",
-          "Resulted in executive resignations and significant damage to the company's reputation"
-      ]
+    name: "Sony Pictures",
+    clues: [
+        "November 2014, United States, $100 million",
+        "Film production company",
+        "Exposed unreleased movies, confidential information, and more",
+        "Attributed to North Korea, possibly in retaliation for the film 'The Interview'",
+        "Resulted in executive resignations and significant damage to the company's reputation"
+    ]
   },
   {
-      name: "Home Depot",
-      clues: [
-          "September 2014, United States, $194.5 million",
-          "Home improvement retail chain",
-          "Affected around 56 million credit and debit cards",
-          "Exposed card numbers, names, and more",
-          "Point-of-sale malware attack"
-      ]
+    name: "Home Depot",
+    clues: [
+        "September 2014, United States, $194.5 million",
+        "Home improvement retail chain",
+        "Affected around 56 million credit and debit cards",
+        "Exposed card numbers, names, and more",
+        "Point-of-sale malware attack"
+    ]
   },
   {
-      name: "Ashley Madison",
-      clues: [
-          "July 2015, Canada, $11.2 million",
-          "Online dating service for extramarital affairs",
-          "Affected around 32 million user accounts",
-          "Exposed names, addresses, credit card information, and more",
-          "Hacking group 'The Impact Team' claimed responsibility"
-      ]
+    name: "Ashley Madison",
+    clues: [
+        "July 2015, Canada, $11.2 million",
+        "Online dating service for extramarital affairs",
+        "Affected around 32 million user accounts",
+        "Exposed names, addresses, credit card information, and more",
+        "Hacking group 'The Impact Team' claimed responsibility"
+    ]
   },
   {
-      name: "Heartland Payment Systems",
-      clues: [
-          "January 2009, United States, $140 million",
-          "Payment processing company",
-          "Affected around 130 million credit and debit cards",
-          "Exposed card numbers, names, and more",
-          "At the time, the largest known data breach involving credit card data"
-      ]
+    name: "Heartland Payment Systems",
+    clues: [
+        "January 2009, United States, $140 million",
+        "Payment processing company",
+        "Affected around 130 million credit and debit cards",
+        "Exposed card numbers, names, and more",
+        "At the time, the largest known data breach involving credit card data"
+    ]
   },
   {
     name: "Staples",
@@ -116,7 +116,7 @@ const breaches = [
       "Office supply retailer",
       "Over 1 million credit card numbers were potentially exposed",
       "Malware was installed on the company's point-of-sale systems",
-      "Staples offered free identity protection services to affected customers"
+      "Company offered free identity protection services to affected customers"
     ]
   },
   {
@@ -156,7 +156,7 @@ const breaches = [
       "Convenience store chain",
       "Customer payment card information was exposed",
       "The breach was the result of malware on the company's payment processing servers",
-      "Wawa faced multiple lawsuits as a result of the breach"
+      "Company faced multiple lawsuits as a result of the breach"
     ]
   },
   {
